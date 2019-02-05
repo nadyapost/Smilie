11d76d6c2ec789f9446a9c9067afc166ed32b539
nadyapost <nadyapostr@gmail.com>
2019-02-05 11:44:38 +1100

    Initial commit

--- Smilie/ViewController.swift
+++ Smilie/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    let sceneView = ARSCNView()
+    let smileLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Device does not support face tracking")
+        }
+        
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { granted in
+            if (granted) {
+                DispatchQueue.main.sync {
+                    // We're going to implement this function in a minute
+                    self.setupSmileTracker()
+                }
+            } else {
+                fatalError("User did not grant camera permission!")
+            }
+        }
     }
 
 
+    func setupSmileTracker() {
+        let configuration = ARFaceTrackingConfiguration()
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
+        view.addSubview(sceneView)
+        
+        smileLabel.text = "😐"
+        smileLabel.font = UIFont.systemFont(ofSize: 150)
+        view.addSubview(smileLabel)
+        // Set constraints
+        smileLabel.translatesAutoresizingMaskIntoConstraints = false
+        smileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        smileLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+    }
+    
+    func renderer(_renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // 1
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        // 2
+        let leftSmileValue = faceAnchor.blendShapes[.mouthSmileLeft] as! CGFloat
+        let rightSmileValue = faceAnchor.blendShapes[.mouthSmileRight] as! CGFloat
+        // 3
+        print(leftSmileValue, rightSmileValue)
+    }
+    
+    
+    
+    
 }
 
