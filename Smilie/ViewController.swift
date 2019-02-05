81317f5cc90b073eb2e1566d75685da5be1771d2
nadyapost <nadyapostr@gmail.com>
2019-02-05 12:14:01 +1100

    Add Comments.

--- Smilie/ViewController.swift
+++ Smilie/ViewController.swift
@@ -17,10 +17,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Make sure your device supports ARKit
         
         guard ARFaceTrackingConfiguration.isSupported else {
             fatalError("Device does not support face tracking")
         }
+        
+        // Get permission to access your device’s camera
         
         AVCaptureDevice.requestAccess(for: AVMediaType.video) { granted in
             if (granted) {
@@ -32,15 +35,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 fatalError("User did not grant camera permission!")
             }
         }
+        // Add Privacy — Camera Usage Description into info.plist
     }
-
-
+    
+    //  Handle face tracking
     func setupSmileTracker() {
         let configuration = ARFaceTrackingConfiguration()
         sceneView.session.run(configuration)
         sceneView.delegate = self
         view.addSubview(sceneView)
         
+        //  basic UI properties to our smileLabel and setting its constraints so it is in the middle of the screen
         smileLabel.text = "😐"
         smileLabel.font = UIFont.systemFont(ofSize: 150)
         view.addSubview(smileLabel)
@@ -48,19 +53,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         smileLabel.translatesAutoresizingMaskIntoConstraints = false
         smileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         smileLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-
+        
     }
+    
     
     func renderer(_renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        // 1
+        // 1 renderer will run every time our scene updates and provides us with the ARAnchor that corresponds to the user’s face
         guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        // 2
+        
+        
+        // 2 blendShapes is a dictionary that stores coefficients corresponding to various facial features
         let leftSmileValue = faceAnchor.blendShapes[.mouthSmileLeft] as! CGFloat
         let rightSmileValue = faceAnchor.blendShapes[.mouthSmileRight] as! CGFloat
+        
+        
         // 3
-        print(leftSmileValue, rightSmileValue)
+        //print(leftSmileValue, rightSmileValue)
+        
+        DispatchQueue.main.async {
+            self.handleSmile(leftValue: leftSmileValue, rightValue: rightSmileValue)
+        }
     }
     
+    // change the emoji in our smileLabel depending on how much the user is smiling into the camera
     func handleSmile(leftValue: CGFloat, rightValue: CGFloat) {
         let smileValue = (leftValue + rightValue)/2.0
         switch smileValue {
